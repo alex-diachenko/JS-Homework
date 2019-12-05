aa055aec9c448aa02a16e085233cfb89b3fc7130
alex-diachenko <55625094+alex-diachenko@users.noreply.github.com>
2019-12-05 17:09:03 +0200

    Create server.js

---
+++ task16/server.js
@@ -0,0 +1,36 @@
+// запрос из хрома на сервер
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('POST', 'http://localhost:3000/answer');
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.send(JSON.stringify({ name: 'SERGEi', lastName: 'SHAKHOV' }));
+
+xhr.onload = function() {
+  if (xhr.status == 200) { 
+	console.log(JSON.parse(xhr.responseText))
+  }
+};
+
+
+
+// сервер
+
+const http = require('http'); // подключаем http модуль!
+http.createServer((req, res) => {
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Headers', '*');
+     if(req.url === '/data') {
+          res.end('success');
+     } else if(req.url === '/answer') {
+          let body = '';
+          req.on('data', (chunk) => {
+               body = String(chunk);
+               console.log(JSON.parse(body));
+          }).on('end', () => {
+               res.end(body);
+          })
+     }else  res.end('HELLO WORLD');
+
+}).listen(3000, '127.0.0.1', () => console.log('Server is listening on port:' + 3000) );
