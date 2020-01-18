eed3fcf73920d67436737179e8f179ff095191f4
alex-diachenko <55625094+alex-diachenko@users.noreply.github.com>
2020-01-18 19:44:53 +0200

    Add files via upload

---
+++ task19/nestjs/dist/app.service.d.ts
@@ -0,0 +1,6 @@
+export declare class AppService {
+    getFormula(): any;
+    addObj(object: any): any;
+    deleteObj(Id: number): string;
+    private paramObjIndex;
+}
---
+++ task19/nestjs/dist/app.service.js
@@ -0,0 +1,124 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const common_1 = require("@nestjs/common");
+let formula = {
+    "parameters": [
+        {
+            "id": 703,
+            "validation": {
+                "min": 0,
+                "max": 100,
+                "required": null,
+                "readonly": null,
+                "params": null
+            },
+            "default": "10",
+            "name": "WF",
+            "label": "Waste Factor"
+        },
+        {
+            "id": 702,
+            "validation": {
+                "min": 0,
+                "max": 60,
+                "required": null,
+                "readonly": null,
+                "params": null
+            },
+            "default": "10",
+            "name": "FILL",
+            "label": "Cable/Pipe FILL/occupancy FILL"
+        },
+        {
+            "id": 701,
+            "validation": {
+                "min": null,
+                "max": null,
+                "required": null,
+                "readonly": true,
+                "params": null
+            },
+            "default": "150",
+            "name": "T",
+            "label": "Seal Thickness"
+        },
+        {
+            "id": 700,
+            "validation": {
+                "min": null,
+                "max": 400,
+                "required": null,
+                "readonly": null,
+                "params": null
+            },
+            "default": "200",
+            "name": "W",
+            "label": "Opening Width"
+        },
+        {
+            "id": 698,
+            "validation": {
+                "min": null,
+                "max": 400,
+                "required": null,
+                "readonly": null,
+                "params": null
+            },
+            "default": "200",
+            "name": "H",
+            "label": "Opening Height"
+        },
+        {
+            "id": 696,
+            "validation": {
+                "min": null,
+                "max": null,
+                "required": null,
+                "readonly": null,
+                "params": null
+            },
+            "default": "1",
+            "name": "OP",
+            "label": "No of Openings"
+        }
+    ],
+    "id": 162,
+    "uuid": "a31ebb13-43db-4ed6-ba47-d026fb899c8f",
+    "createdAt": "15:30:18.896638+00",
+    "updatedAt": "15:30:18.896638+00",
+    "name": "rectangular",
+    "formula": "round(OP * (H * W * T) / 1000 / 1000 * (100 - FILL) / 100 / 2.1 * (100 + WF) / 100, 1)",
+};
+let AppService = class AppService {
+    getFormula() {
+        return formula;
+    }
+    addObj(object) {
+        formula.parameters.push(object);
+        console.log(`added object with id : ${object.id}`);
+        return formula;
+    }
+    deleteObj(Id) {
+        const index = this.paramObjIndex(Id);
+        formula.parameters.splice(index, 1);
+        return `removed object with this id: ${Id}`;
+    }
+    paramObjIndex(id) {
+        const objectIndex = formula.parameters.findIndex((obj) => obj.id == id);
+        if (objectIndex === -1) {
+            throw new common_1.NotFoundException('Could not find this id');
+        }
+        return objectIndex;
+    }
+};
+AppService = __decorate([
+    common_1.Injectable()
+], AppService);
+exports.AppService = AppService;
+//# sourceMappingURL=app.service.js.map
